56ce40943e34b24023f8e9f345a8a491240f50c7
mauricio <mauricio.vera.r7@gmail.com>
2021-09-13 00:59:24 -0300

    ver.1

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="App">
+    <form>
+      <label for="listado">Tarea</label>
+
+      <input type="text"     v-model="texto"  id="listado"   placeholder="ingresa nueva tarea"/>
+      <div v-if="texto">
+      <button class="btn" type="button" @click.prevent="guardar">Añade</button>
+      </div>
+    </form>
+    
+    
+    <Listado :lista="lista" @eliminarItem="eliminarItem" />
+    
+  </div>
+</template>
+
+<script>
+import Listado from "./Listado.vue";
+
+export default {
+  name: "App",
+  components: {
+    Listado,
+  },
+
+  data() {
+    return {
+      lista: [],
+      texto: '',
+    };
+  },
+
+  methods: {
+    guardar() {
+      this.lista.push(this.texto);
+      this.texto = '';               
+
+    },
+
+    eliminarItem(index) {
+      this.lista.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+#App {
+  padding: 40px 400px;
+}
+button {
+  border-radius: 12%;  
+  border: 1px solid black;  
+  margin: 2px;
+}
+button:hover {
+    border: 4px solid rgb(122, 167, 204);
+
+}
+.btn {
+  position: absolute;
+    margin: -30px 0 0 300px;
+  }
+</style>
---
+++ src/Listado.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="(item, index) in lista" :key="index">
+          {{ lista[index] }}
+
+          <button @click="eliminar(index)">eliminar</button>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: "Listado",
+  props: {
+    lista: [],
+    
+  },
+
+  methods: {
+    eliminar(index) {
+      this.$emit("eliminarItem", index);
+    },
+  },
+};
+</script>
+
+
